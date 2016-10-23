db67ab24ee42723812de148f847585624c015821
Lucas <rukaso.mero@gmail.com>
2016-10-23 19:16:31 -0200

    update

---
+++ src/app/models/model-venda/venda.class.ts
@@ -0,0 +1,15 @@
+export class Venda {
+    
+    codigo?: number;
+    dataInicio: any; // required with minimum 5 chracters
+    dataFim: any;
+    quantidadeItens : any;
+    status : string;
+    valorTotal?: number;
+    codigoCliente: number;
+
+    constructor (){
+
+    }
+
+}
