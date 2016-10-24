ef219170e18a33d3009c0aaec946812d8c3ddf10
Lucas <rukaso.mero@gmail.com>
2016-10-23 22:33:07 -0200

    update

--- src/app/models/model-compra/compra.class.ts
+++ src/app/models/model-compra/compra.class.ts
@@ -1,7 +1,7 @@
 export class Compra {
     
     codigo?: number;
-    dataInicio: any; // required with minimum 5 chracters
+    dataInicio: any; 
     dataFim: any;
     quantidadeItens : any;
     status : string;
